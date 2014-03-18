9405c0c1fbfccb13ae5d42cd7eaa9eb001958669
Daniel Cochran <daniel@blusteel.com>
2014-03-17 23:19:08 -0700

    First Activity

---
+++ ESOPAPP/src/com/example/esopapp/main.java
@@ -0,0 +1,16 @@
+package com.example.esopapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {         
+
+       super.onCreate(savedInstanceState);    
+       setContentView(R.layout.main_view);
+       //rest of the code
+   }
+
+}
